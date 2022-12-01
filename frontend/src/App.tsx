31769b9640a0c97aa5db63780dad302ff284af7c
marina <marina-dominguez@alumni.nmu.edu>
2022-12-01 16:36:18 -0500

    Location Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import { useRef, useEffect } from 'react';
-import { Place } from './models/Place';
-import { getPlaces } from '../src/services/apiPull';
+import  Map  from './components/map';
+
 
 function App() {
   
-  const [places, setPlaces] = useState<Place>();
-  useEffect(() => {
-    getPlaces().then((res) => {
-      const {data} = res;
-      setPlaces(data);
-      console.log(res.data);
-    })
-  }, []);
   return (
-    <div className="App">
-      <header>Hi</header>
-    </div>
+      <Map/>
   );
 }
 export default App;
--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -1,5 +1,6 @@
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
-import { useState } from 'react'; 
+import React, { useMemo } from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { useState, useEffect } from 'react';
 
 interface Coordinates {
   lat: number;
@@ -10,32 +11,38 @@ export default function Map(){
   const [pos, setPos] = useState<Coordinates>();
   
   function geoLocation (){
+    console.log(navigator.geolocation) //test
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position: GeolocationPosition) => {
-          const Coordinates = {
+          const TheCoordinates = { //this was originally Coordinates
             lat: position.coords.latitude,
             lng: position.coords.longitude,
-        }
-        setPos(Coordinates);
-        });
-      }};
+          }
+        setPos(TheCoordinates);
+        //console.log("Navigator Current Position is: " + TheCoordinates)
+      });
+    }
+  };
+
    const {isLoaded, loadError} = useLoadScript({
      googleMapsApiKey: process.env.GoogleApiKey!,
      libraries: ["places"],
    });
- 
+
    if (loadError) return <div>Error Loading Map</div>
-   if (!isLoaded) return( <><div>Loading Maps</div>
-   <button onClick={geoLocation}></button></>)
+   if (!isLoaded) return <div>Loading Maps</div>
  
    return (
      <div> 
+       {/* When button is click function Geolocation runs */}
+       <button className='Geolocation-btn' onClick={geoLocation}>Click to locate</button>
        <GoogleMap 
        mapContainerClassName="map-container"
-       zoom={10} 
+       zoom={15} 
        center={pos}
        >
+         {/* <Marker position={geoLocation}/> */}
        </GoogleMap>
  
      </div>
