3d49e3921c3211b21d08f95286c56bbdeb9f2b64
marina <marina-dominguez@alumni.nmu.edu>
2022-12-02 15:07:51 -0500

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Map from './Components/map';
+import Map from './components/map';
 
 interface pos{
     pos: number;
@@ -11,17 +10,12 @@ interface Coordinates {
     lat: number;
     lng: number;
 };
-=======
-import  Map  from './components/map';
-
->>>>>>> 31769b9640a0c97aa5db63780dad302ff284af7c
 
 function App() {
     
 
     const [pos, setPos] = useState<Coordinates>();
   
-<<<<<<< HEAD
     useEffect(() => {
 
       if (navigator.geolocation) {
@@ -39,10 +33,6 @@ function App() {
     
    <Map  pos= {pos}/>
 
-=======
-  return (
-      <Map/>
->>>>>>> 31769b9640a0c97aa5db63780dad302ff284af7c
   );
 };
 
---
+++ frontend/src/components/Nearby.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+// import { useQuery } from 'react-query';
+
+// const PLACE_RADUIS = 2500; //2500 meters
+// const cafe = 'cafe';
+// const artGallery = 'art_gallery';
+// const movieTheater = 'movie_theater';
+// const restaurant = 'restaurant';
+// const spa = 'spa';
+
+// export const fetchNearbyPlaces = async (lat: number, lng: number): Promise<MarkerType[]>
