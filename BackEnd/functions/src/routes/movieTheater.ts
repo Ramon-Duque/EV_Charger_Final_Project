92ee5412bc926e40e6a65fbb2547b0a5c6fdae74
marina <marina-dominguez@alumni.nmu.edu>
2022-12-07 20:03:23 -0500

    made addition activity routes

---
+++ BackEnd/functions/src/routes/movieTheater.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import axios from "axios";
+ 
+const movieTheaterRoutes = Router();
+ 
+const apiKey = process.env.REACT_APP_API_KEY!;
+let latitude = 42.972191;
+let longitude = -85.610359;
+
+movieTheaterRoutes.get('/restaurants', async (req, res) => {
+   const response = await axios.get(
+     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=2000&keyword=movie_theater&key=${apiKey}`
+   );
+   res.json(response.data);
+ });
+ 
+ 
+ 
+export default movieTheaterRoutes;
---
+++ BackEnd/functions/src/routes/spa.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import axios from "axios";
+ 
+const spaRoutes = Router();
+ 
+const apiKey = process.env.REACT_APP_API_KEY!;
+let latitude = 42.972191;
+let longitude = -85.610359;
+
+spaRoutes.get('/restaurants', async (req, res) => {
+   const response = await axios.get(
+     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=2000&keyword=spa&key=${apiKey}`
+   );
+   res.json(response.data);
+ });
+ 
+ 
+ 
+export default spaRoutes;
