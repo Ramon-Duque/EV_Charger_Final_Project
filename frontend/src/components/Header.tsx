c60b514b75ef7b47b436fdce4b87947a69bc6da3
Ramon Duque <ramonduke@gmail.com>
2022-12-14 12:56:02 -0500

    icons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../CSS/Header.css';
 import { Link } from 'react-router-dom'
+import {WiLightning} from 'weather-icons-react'
 
 
 
@@ -12,7 +13,7 @@ export const Header = () => {
 
     return (
         <div className='nav-bar'>
-            <h1 id='ECharge'>E Charge</h1>
+            <h1 id='ECharge'>E Charge <WiLightning size={50} color='FFFF'/></h1>
                 <ul>
                     <li><Link to={'/'}><button className='nav-button' id='home'>Home</button></Link></li>
                     <li><Link to={'/favorites'}><button className='nav-button' id='home'>Favorites</button></Link></li>
